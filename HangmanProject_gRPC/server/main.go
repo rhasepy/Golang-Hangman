9aa130ba74339e5438d0b08b15510725a942a4b5
rhasepy <ozannyesiller@gmail.com>
2022-08-23 13:28:50 +0300

    Up

--- HangmanProject_gRPC/server/main.go
+++ HangmanProject_gRPC/server/main.go
@@ -1,53 +1,14 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"google.golang.org/grpc"
 	"hangman_grpc/api"
 	"hangman_grpc/util"
-	"math/rand"
 	"net"
 	"os"
+
+	"google.golang.org/grpc"
 )
-
-type myWeatherService struct {
-	api.UnimplementedWeatherServiceServer
-}
-
-func (m *myWeatherService) ListCities(ctx context.Context, req *api.ListCitiesRequest) (*api.ListCitiesResponse, error) {
-	return &api.ListCitiesResponse{
-		Items: []*api.CityEntry{
-			&api.CityEntry{CityCode: "tr_ank",
-				CityName: "Ankara"},
-			&api.CityEntry{CityCode: "tr_ist",
-				CityName: "Istanbul"},
-		},
-	}, nil
-}
-
-func (m *myWeatherService) QueryWeather(req *api.WeatherRequest, resp api.WeatherService_QueryWeatherServer) error {
-	for {
-		err := resp.Send(&api.WeatherResponse{Temperature: rand.Float32()*10 + 10})
-		if err != nil {
-			break
-		}
-	}
-	return nil
-}
-
-func WeatherServiceStart() {
-
-	lis, err := net.Listen("tcp", "localhost:8080")
-	if err != nil {
-		panic(err)
-	}
-
-	srv := grpc.NewServer()
-	api.RegisterWeatherServiceServer(srv, &myWeatherService{})
-	fmt.Println("Starting server...")
-	panic(srv.Serve(lis))
-}
 
 func main() {
 
